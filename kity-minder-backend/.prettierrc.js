63d0a1d61a93b39c0d7eaec72ebea03fbcb9f760
Hstarorg <hm910705@163.com>
2018-03-26 13:09:19 +0800

    Update deps

---
+++ kity-minder-backend/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  parser: 'babylon',
+  printWidth: 120, // 每行最大120字符
+  semi: true, // 末尾分号
+  singleQuote: true // 使用单引号
+};
---
+++ kity-minder-frontend/build/gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const shelljs = require('shelljs');
+
+gulp.task('serve', done => {});
+
+gulp.task('restart', done => {});
+
+gulp.task('watch', done => {});
+
+gulp.task('dev', gulp.series('watch'));
