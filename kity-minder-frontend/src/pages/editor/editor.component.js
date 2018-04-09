2e2f3d878d2ca1bb3b473760371174e5adf2c647
Hstarorg <hm910705@163.com>
2018-04-10 00:40:06 +0800

    Add global key bind

--- kity-minder-frontend/src/pages/editor/editor.component.js
+++ kity-minder-frontend/src/pages/editor/editor.component.js
@@ -1,10 +1,31 @@
+import { messageBox } from '../../common';
 export class EditorComponent {
   static selector = 'editorComponent';
   static template = require('./editor.html');
-  constructor() {}
+  static $inject = ['$location'];
+  constructor($location) {
+    this.$location = $location;
+  }
+
+  editor = null;
+  minder = null;
 
   initEditor(editor, minder) {
-    window.editor = editor;
-    window.minder = minder;
+    this.editor = editor;
+    this.minder = minder;
   }
+
+  saveMinderData = e => {
+    e.preventDefault();
+    const path = this.$location.path();
+    if (path.startsWith('/minder/')) {
+      messageBox.msg('保存成功');
+    }
+  };
+
+  $onInit() {
+    Mousetrap.bindGlobal('ctrl+s', this.saveMinderData);
+  }
+
+  $onDestroy() {}
 }
